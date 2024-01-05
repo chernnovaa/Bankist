f31d32a21c882beffe861294dfa00af380d22e79
chernnovaa <katac0747@gmail.com>
2024-01-06 00:07:03 +0200

    Fix bugs and add a cookie message

--- js/script.js
+++ js/script.js
@@ -1,1 +1,73 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector('.header');
+
+///////////////////////////////////////
+// Modal window
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+///////////////////////////////////////
+// Cookie message
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML = `Cookie message. <button class="btn btn--close-cookie">Got it !</button>`;
+header.append(message);
+
+document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+  message.parentElement.removeChild(message);
+});
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+///////////////////////////////////////
+// Smooth scrolling
+const btnScroolTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScroolTo.addEventListener('click', function (e) {
+  const s1coordinate = section1.getBoundingClientRect();
+  console.log(s1coordinate);
+  console.log(e.target.getBoundingClientRect());
+  console.log('Current scrool (X/Y)', window.scrollX, window.scrollY);
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // window.scrollTo({
+  //   left: s1coordinate.left + window.scrollX,
+  //   top: s1coordinate.top + window.scrollY,
+  //   behavior: 'smooth',
+  // }); - (old school calculation)
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+///////////////////////////////////////
+// Navigation effect (Event Propagation)
